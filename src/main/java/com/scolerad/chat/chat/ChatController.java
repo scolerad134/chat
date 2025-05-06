fc7b35ed2a50b9efad521b92dee7374570aa7464
scolerad134 <gugac134@gmail.com>
2025-05-06 20:08:44 +0300

    add chat controller

---
+++ src/main/java/com/scolerad/chat/chat/ChatController.java
@@ -0,0 +1,28 @@
+package com.scolerad.chat.chat;
+
+import com.scolerad.chat.model.ChatMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ChatController {
+
+    @MessageMapping("/chat.sendMessage")
+    @SendTo("/topic/public")
+    public ChatMessage send(@Payload ChatMessage chatMessage) {
+        return chatMessage;
+    }
+
+    @MessageMapping("/chat.addUser")
+    @SendTo("/topic/public")
+    public ChatMessage addUser(
+        @Payload ChatMessage chatMessage,
+        SimpMessageHeaderAccessor headerAccessor
+    ) {
+        headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
+        return chatMessage;
+    }
+}
---
+++ src/main/java/com/scolerad/chat/enums/MessageType.java
@@ -0,0 +1,8 @@
+package com.scolerad.chat.enums;
+
+public enum MessageType {
+
+    CHAT,
+    JOIN,
+    LEAVE
+}
