d871c10530399f93eefc9b136b9a153cf764717d
scolerad134 <gugac134@gmail.com>
2025-05-06 20:24:27 +0300

    add listener to send message about disconnection user

--- src/main/java/com/scolerad/chat/config/WebSocketEventListener.java
+++ src/main/java/com/scolerad/chat/config/WebSocketEventListener.java
@@ -1,8 +1,12 @@
 package com.scolerad.chat.config;
 
+import com.scolerad.chat.enums.MessageType;
+import com.scolerad.chat.model.ChatMessage;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 
@@ -11,8 +15,21 @@ import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 @Slf4j
 public class WebSocketEventListener {
 
+    private final SimpMessageSendingOperations messageTemplate;
+
     @EventListener
     public void HandleWebSocketDisconnectListener(SessionDisconnectEvent event) {
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+        String username = (String) headerAccessor.getSessionAttributes().get("username");
 
+        if (username != null) {
+            log.info("User disconnected: {}", username);
+            ChatMessage chatMessage = ChatMessage.builder()
+                .type(MessageType.LEAVE)
+                .sender(username)
+                .build();
+
+            messageTemplate.convertAndSend("/topic/public", chatMessage);
+        }
     }
 }
